e5437a13f60c1e42d053602fc19cba45785a1ae6
Tomaz Vieira <tom.fogaca@gmail.com>
2024-10-29 15:49:02 +0100

    Adds more documentation and doctests

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@
 //!
 //! Urls are often used as strings, but what they really are is a serialized
 //! structure with fields such as `scheme`, `hostname`, and `query_params`.
+//! In fact, `murl` URLs do _not_ contain their string representation inside them.
 //!
 //! This crate provides the `Url` struct, which should be the preferred
 //! way to create and modify URLs instead of fallibly parsing and/or concatenating
@@ -10,34 +11,62 @@
 //! <br>
 //! ## Examples
 //!
-//! ### Infallibly creating a url
+//! ### Infallibly creating a URL
 //! ```rust
-//!    use std::str::FromStr;
-//!    use std::collections::BTreeMap;
-//!    use murl::{Url, Protocol, Host, Label};
-//!    use camino::Utf8PathBuf;
-//!    // non-fallibly creating the url
-//!    let mut url = Url{
-//!        protocol: Protocol::Https,
-//!        host: Host { // the hostname is e"xample.com"
-//!            name: Label::from_str("example").unwrap(),
-//!            domains:  vec![
-//!                Label::from_str("com").unwrap(),
-//!            ]
-//!        },
-//!        port: Some(443),
-//!        path: Utf8PathBuf::from_str("/some/path").unwrap(),
-//!        query: BTreeMap::from([ // query params are just raw strings. Escaping is done automatically
-//!            ("key with spaces".into(), "val&with&ampersands".into()),
-//!            ("key=with=equals".into(), "val#with#hashtag".into()),
-//!        ]),
-//!        fragment: None,
-//!    };
-//!    assert_eq!(
-//!        url.to_string(),
-//!        "https://example.com:443/some/path?key%20with%20spaces=val%26with%26ampersands&key%3Dwith%3Dequals=val%23with%23hashtag"
-//!    );
+//!use std::str::FromStr;
+//!use std::collections::BTreeMap;
+//!use murl::{Url, Scheme, Host, Label};
+//!use camino::Utf8PathBuf;
+//!// non-fallibly creating the url
+//! let mut url = Url{
+//!     scheme: Scheme::Https,
+//!     host: Host { // the hostname is "example.com"
+//!         name: Label::from_str("example").unwrap(),
+//!         domains:  vec![
+//!             Label::from_str("com").unwrap(),
+//!         ]
+//!     },
+//!     port: Some(443),
+//!     path: Utf8PathBuf::from("/some/path"),
+//!     query: BTreeMap::from([ // query params are just strings. Escaping is done automatically
+//!         ("key with spaces".into(), "val&with&ampersands".into()),
+//!         ("key=with=equals".into(), "val#with#hashtag".into()),
+//!     ]),
+//!     fragment: None,
+//! };
+//! assert_eq!(
+//!     url.to_string(),
+//!     "https://example.com:443/some/path?key%20with%20spaces=val%26with%26ampersands&key%3Dwith%3Dequals=val%23with%23hashtag"
+//! );
 //!```
+//!
+//! ### Parsing a URL from a string
+//! If you get a string from a user or an external process, you can fallibly parse it via `FromStr`:
+//!
+//! ```rust
+//! use std::str::FromStr;
+//! use std::collections::BTreeMap;
+//! use murl::{Url, Scheme, Host, Label};
+//! use camino::Utf8PathBuf;
+//!
+//! let parsed_url = Url::from_str("http://example.com/some/path?a=123").unwrap();
+//! let expected = Url{
+//!     scheme: Scheme::Http,
+//!     host: Host{
+//!         name: Label::from_str("example").unwrap(),
+//!         domains: vec![
+//!             Label::from_str("com").unwrap()
+//!         ],
+//!     },
+//!     port: None,
+//!     path: Utf8PathBuf::from("/some/path"),
+//!     query: BTreeMap::from([
+//!         ("a".to_owned(), "123".to_owned())
+//!     ]),
+//!     fragment: None,
+//! };
+//! assert_eq!(parsed_url, expected);
+//! ```
 
 pub use camino;
 use percent_encoding::percent_decode_str;
@@ -93,6 +122,9 @@ pub enum LabelError{
 }
 
 #[derive(Clone, PartialEq, Eq, Debug)]
+/// Each of the period-separated components of a hostname.
+///
+/// E.g. `example` and `com` in `example.com`
 pub struct Label(String);
 
 impl Display for Label{
@@ -102,6 +134,7 @@ impl Display for Label{
 }
 
 impl Label{
+    /// Parses `input` until a `Label` is found, and returns the remaining input if successful.
     pub fn parse(input: &str) -> Result<(Self, &str), LabelError>{
         let (input, rest) = {
             match input.find(|c| "/.:".contains(c)){
@@ -139,8 +172,11 @@ impl FromStr for Label{
 }
 
 #[derive(Clone, PartialEq, Eq, Debug)]
+/// A host name like `example.com`
 pub struct Host{
+    // The required, leftmost `Label` of the hostname, like `vm1` in `vm1.example.com`
     pub name: Label,
+    // Optional domains where the host is, like `example.com` in `vm1.example.com`
     pub domains: Vec<Label>,
 }
 
